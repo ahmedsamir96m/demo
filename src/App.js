7850cb138b8a060f3036c8e70efec65dc12f1961
unknown <ahmedsamir96m@gmail.com>
2022-10-18 12:47:01 +0200

    created components skeletons, NavBar, Logo, MainSlider, Products, Luxury, Partners, Footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { Navbar } from "./components/navbar/Navbar";
+import { MainSlider } from "./components/mainSlider/MainSlider";
+import { Products } from "./components/products/Products";
+import { Luxury } from "./components/luxury/Luxury";
+import { Partners } from "./components/partners/Partners";
+import { Footer } from "./components/footer/Footer";
+
 function App() {
   return (
     <div className="App">
-      <h1>App Skeleton</h1>
+      <Navbar />
+      <MainSlider />
+      <Products />
+      <Luxury />
+      <Partners />
+      <Footer />
     </div>
   );
 }
