464fe303a6ff6f047e5a3e13aabdb505020d8e80
unknown <ahmedsamir96m@gmail.com>
2022-10-18 13:44:30 +0200

    created brand logo component

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export const Logo = () => {
 	return (
-		<div>Logo</div>
+		<div>
+			  <a className="navbar-brand" href="/">
+					<img src="../../assets/global/logo.png" alt="IStandard Logo" />
+				</a>
+		</div>
 	)
 }
